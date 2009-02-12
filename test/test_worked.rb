5ea4aff14b775f39d0954b84443c64ed0470e649
Mirko Stocker <me@misto.ch>
2009-02-12 15:46:31 +0100

    Tests with minutes

--- test/test_worked.rb
+++ test/test_worked.rb
@@ -3,9 +3,10 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestWorked < Test::Unit::TestCase
 
   def test_hours_and_activity
-    assert_record 5.hours, "coding", "5 hours on coding"
-    assert_record 5.hours, "coding and writing", "4 to 9 on coding and writing"
+    assert_record 5.hours,              "coding",             "5 hours on coding"
+    assert_record 5.hours,              "coding and writing", "4 to 9 on coding and writing"
     assert_record 5.hours + 10.minutes, "coding and writing", "4 to 9:10 on coding and writing"
+    assert_record 5.hours +  5.minutes, "coding",             "5 hours 5 minutes on coding"
   end
 
   def assert_record exp_duration, exp_activity, source
